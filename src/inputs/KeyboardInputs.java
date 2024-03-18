cdb6e3ff21fa124c036fa465b1a02fb48c5f9503
Anon <thunderouspeanut@gmail.com>
2024-03-18 14:57:03 +0530

    jumping and gravity added

--- src/inputs/KeyboardInputs.java
+++ src/inputs/KeyboardInputs.java
@@ -5,47 +5,51 @@ import java.awt.event.KeyListener;
 
 import main.GamePanel;
 
-import static utilz.Constants.Directions.*;
-
-// Class handling keyboard inputs for the game panel
 public class KeyboardInputs implements KeyListener {
 
-    // Field to hold the game panel
     private GamePanel gamePanel;
 
-    // Constructor for the KeyboardInputs class
     public KeyboardInputs(GamePanel gamePanel) {
         this.gamePanel = gamePanel;
     }
 
-    // Method handling key typed event
     @Override
     public void keyTyped(KeyEvent e) {
-        // Not implemented
+        // TODO Auto-generated method stub
     }
 
-    // Method handling key released event
     @Override
     public void keyReleased(KeyEvent e) {
-        // Switch statement to handle released keys
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_W -> gamePanel.getGame().getPlayer().setUp(false); // Set moving flag to false
-            case KeyEvent.VK_A -> gamePanel.getGame().getPlayer().setLeft(false); // Set moving flag to false
-            case KeyEvent.VK_S -> gamePanel.getGame().getPlayer().setDown(false); // Set moving flag to false
-            case KeyEvent.VK_D -> gamePanel.getGame().getPlayer().setRight(false); // Set moving flag to false
+//		case KeyEvent.VK_W:
+//			gamePanel.getGame().getPlayer().setUp(false);
+//			break;
+            case KeyEvent.VK_A -> gamePanel.getGame().getPlayer().setLeft(false);
+
+//		case KeyEvent.VK_S:
+//			gamePanel.getGame().getPlayer().setDown(false);
+//			break;
+            case KeyEvent.VK_D -> gamePanel.getGame().getPlayer().setRight(false);
+//            case KeyEvent.VK_SPACE -> gamePanel.getGame().getPlayer().setJump(false);
+            case KeyEvent.VK_W -> gamePanel.getGame().getPlayer().setJump(false);
         }
     }
 
-    // Method handling key pressed event
     @Override
     public void keyPressed(KeyEvent e) {
-        // Switch statement to handle pressed keys
         switch (e.getKeyCode()) {
-            case KeyEvent.VK_W -> gamePanel.getGame().getPlayer().setUp(true); // Set moving flag to true
-            case KeyEvent.VK_A -> gamePanel.getGame().getPlayer().setLeft(true); // Set moving flag to true
-            case KeyEvent.VK_S -> gamePanel.getGame().getPlayer().setDown(true); // Set moving flag to true
-            case KeyEvent.VK_D -> gamePanel.getGame().getPlayer().setRight(true); // Set moving flag to true
-            case KeyEvent.VK_J -> gamePanel.getGame().getPlayer().setAttacking(true); // Set moving flag to true
+//		case KeyEvent.VK_W:
+//			gamePanel.getGame().getPlayer().setUp(true);
+//			break;
+            case KeyEvent.VK_A -> gamePanel.getGame().getPlayer().setLeft(true);
+
+//		case KeyEvent.VK_S:
+//			gamePanel.getGame().getPlayer().setDown(true);
+//			break;
+            case KeyEvent.VK_D -> gamePanel.getGame().getPlayer().setRight(true);
+            case KeyEvent.VK_J -> gamePanel.getGame().getPlayer().setAttacking(true); // Set lite attack flag to true
+//            case KeyEvent.VK_SPACE -> gamePanel.getGame().getPlayer().setJump(true);
+            case KeyEvent.VK_W -> gamePanel.getGame().getPlayer().setJump(true);
         }
     }
 }
