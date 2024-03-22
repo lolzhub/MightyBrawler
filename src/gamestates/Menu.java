27ca726e1e9a5f4c18aa97c5ea9bfe790feebe75
Anon <thunderouspeanut@gmail.com>
2024-03-22 16:32:02 +0530

    menu added to the game! + bug fixes

--- src/gamestates/Menu.java
+++ src/gamestates/Menu.java
@@ -4,26 +4,53 @@ import java.awt.Color;
 import java.awt.Graphics;
 import java.awt.event.KeyEvent;
 import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
 
 import main.Game;
+import ui.MenuButton;
+import utilz.LoadSave;
 
 public class Menu extends State implements Statemethods {
 
+    private MenuButton[] buttons = new MenuButton[3];
+    private BufferedImage backgroundImage;
+    private int menuX, menuY, menuWidth, menuHeight;
+
     public Menu(Game game) {
         super(game);
+        loadButtons();
+        loadBackground();
+    }
 
+    private void loadBackground() {
+        backgroundImage = LoadSave.GetSpriteAtlas(LoadSave.MENU_BACKGROUND);
+        menuWidth = (int) (backgroundImage.getWidth() * Game.SCALE);
+        menuHeight = (int) (backgroundImage.getHeight() * Game.SCALE);
+        menuX=Game.GAME_WIDTH/2 - menuWidth/2;
+        menuY=(int) (45* Game.SCALE);
+    }
+
+    private void loadButtons() {
+        buttons[0]= new MenuButton(Game.GAME_WIDTH/2, (int) (150*Game.SCALE),0, Gamestate.PLAYING);
+        buttons[1]= new MenuButton(Game.GAME_WIDTH/2, (int) (220*Game.SCALE),1, Gamestate.OPTIONS);
+        buttons[2]= new MenuButton(Game.GAME_WIDTH/2, (int) (290*Game.SCALE),2, Gamestate.QUIT);
     }
 
     @Override
     public void update() {
+        for (MenuButton mb: buttons){
+            mb.update();
+        }
 
     }
 
     @Override
     public void draw(Graphics g) {
+        g.drawImage(backgroundImage, menuX, menuY, menuWidth, menuHeight, null);
+        for (MenuButton mb: buttons){
+            mb.draw(g);
+        }
 
-        g.setColor(Color.black);
-        g.drawString("MENU", Game.GAME_WIDTH / 2, 200);
     }
 
     @Override
@@ -34,19 +61,46 @@ public class Menu extends State implements Statemethods {
 
     @Override
     public void mousePressed(MouseEvent e) {
-        // TODO Auto-generated method stub
+        for (MenuButton mb: buttons){
+            if(isIn(e, mb)){
+                mb.setMousePressed(true);
+                break;
+            }
+        }
 
     }
 
     @Override
     public void mouseReleased(MouseEvent e) {
-        // TODO Auto-generated method stub
+        for (MenuButton mb: buttons){
+            if(isIn(e, mb)){
+                if(mb.isMousePressed()){
+                    mb.applyGameState();
+                }
+                break;
+            }
+        }
+        resetButtons();
 
+    }
+
+    private void resetButtons() {
+        for (MenuButton mb: buttons){
+            mb.resetBools();
+        }
     }
 
     @Override
     public void mouseMoved(MouseEvent e) {
-        // TODO Auto-generated method stub
+        for (MenuButton mb: buttons){
+            mb.setMouseOver(false);
+        }
+        for (MenuButton mb: buttons){
+            if (isIn(e, mb)){
+                mb.setMouseOver(true);
+                break;
+            }
+        }
 
     }
 
--- src/utilz/Constants.java
+++ src/utilz/Constants.java
@@ -1,7 +1,18 @@
 package utilz;
 
+import main.Game;
+
 // Class containing constant values used in the game
 public class Constants {
+
+    public static class UI{
+        public static class Buttons{
+            public static final int B_WIDTH_DEFAULT=140;
+            public static final int B_HEIGHT_DEFAULT=56;
+            public static final int B_WIDTH=(int)(B_WIDTH_DEFAULT* Game.SCALE);
+            public static final int B_HEIGHT=(int) (B_HEIGHT_DEFAULT* Game.SCALE);
+        }
+    }
 
     // Inner class for directions constants
     public static class Directions {
